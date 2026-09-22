e5487f0c578efa3ece91bfa3cae676ee42568e64
Felipe-tecnico-cmyk <felipetecnico911@gmail.com>
2026-09-22 08:44:44 -0300

    Update main.py

--- GastosApplication/main.py
+++ GastosApplication/main.py
@@ -28,14 +28,18 @@ def adicionar_gasto():
     descricao = input("Digite a descrição do gasto: ")
     try:
         valor = float(input("Digite o valor: "))
-        if valor <= 0:
+        if valor > 0:
+            categoria = input("Digite a categoria: ").lower()
+            data = input("Digite a data (AAAA/MM/DD): ")
+            pass
+        else:
             print("Erro: Digite um número positivo!")
-        return valor
+            return valor
+        
     except ValueError:
         print("Erro: Digite um valor válido!")
-        return valor
-    categoria = input("Digite a categoria: ").lower()
-    data = input("Digite a data (AAAA/MM/DD): ")
+        return 
+    
 
     novo_gasto = {
         "id": len(gastos) + 1,
